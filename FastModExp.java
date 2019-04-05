03ec2dfa8c5cd6d9a88a6750827e35cdead1c712
DJXi <47097477+DJXi@users.noreply.github.com>
2019-04-04 20:40:50 -0500

    Update FastModExp.java

--- FastModExp.java
+++ FastModExp.java
@@ -3,8 +3,18 @@ package assignment6;
 public class FastModExp {
 
   public static int fastModExp(int x, int y, int m) {
-    if (y == 1) {
-      return x % m;
+    if (y == 0) {
+      if (java.lang.Math.abs(m) != 1) {
+        return 1;
+      } else {
+        return 0;
+      } // if/else
+    } else if (y < 0) {
+      if ((java.lang.Math.abs(x) == 1) && (java.lang.Math.abs(m) != 1)) {
+        return x;
+      } else {
+        return 0;
+      } // if/else
     } else if ((y % 2) == 0) {
       return FastModExp.fastModExp(x * x % m, y / 2, m);
     } else {
